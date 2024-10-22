c870393f2c306aa6ac7cfd41d1820669a6daa479
brayan anthony alaya atalaya <brayan.alaya@hotmail.com>
2024-10-22 16:24:04 -0500

    skills added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componentes/header/header.component";
 import { MainComponent } from './componentes/main/main.component';
-import { FooterComponent } from "./componentes/footer/footer.component";
+import { SkillsComponent } from "./componentes/skills/skills.component";
+import { ProjectsComponent } from "./componentes/projects/projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
